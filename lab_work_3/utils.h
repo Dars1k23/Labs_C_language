ae96efa4003d9071d12484f5eab96748bb8593a1
Dars1k23 <sanchous-2017@mail.ru>
2026-01-16 23:17:44 +0300

    Create utils.h

---
+++ lab_work_3/utils.h
@@ -0,0 +1,10 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+void ad_ellement(int **list, int *size, int *cap, int new_ellement);
+int sum_digits(int num);
+int* my_task(int **list, int *size, int *new_size, int M);
+void print_array(int *list, int size);
+int run(void);
+
+#endif
